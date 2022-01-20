27b56df553bac7142e9297ab1543e946bcfc79e9
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-01-20 16:50:38 -0300

    Adicionar arquivos de projeto.

---
+++ while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha == 2002)
+            {
+                Console.WriteLine(senha);
+                Console.Write("Senha correta");
+                break;
+            }
+
+            if (senha != 2002 )
+            Console.WriteLine("Senha Invalida");
+
+        }
+    }
+}
